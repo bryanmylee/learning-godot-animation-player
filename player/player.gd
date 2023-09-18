815e8410102b9ab0d5b3bccc48a431350bf38985
Bryan Lee <bryanmylee@gmail.com>
2023-09-18 12:19:16 +0800

    feat: locking kick animation

--- player/player.gd
+++ player/player.gd
@@ -22,6 +22,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var move_direction := Vector3.FORWARD
 var move_speed := SPEED_WALK
 var is_running := false
+var is_kicking := false
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -31,8 +32,16 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
+func handle_animation_kick_end():
+	is_kicking = false
 
 func _physics_process(delta):
+	
+	if Input.is_action_just_pressed("action_kick"):
+		if animation_player.current_animation != "kick":
+			animation_player.play("kick")
+			is_kicking = true
+	
 	is_running = Input.is_action_pressed("mod_run")
 	
 	# Add the gravity.
@@ -40,27 +49,32 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") \
+		and is_on_floor() \
+		and not is_kicking:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward") \
+		if not is_kicking else Vector2.ZERO
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	move_direction = move_direction.slerp(direction, 8 * delta)
 	visuals.look_at(move_direction + position)
 	
+	# Handle movement acceleration
 	move_speed = move_toward(move_speed, SPEED_RUN if is_running else SPEED_WALK, ACCELERATION * delta)
 	var move_animation := "running" if is_running else "walking" 
 	
-	if direction:
+	if not direction.is_zero_approx():
 		if animation_player.current_animation != move_animation:
 			animation_player.play(move_animation)
 		
 		velocity.x = direction.x * move_speed
 		velocity.z = direction.z * move_speed
 	else:
-		if animation_player.current_animation != "idle":
+		if not is_kicking \
+			and animation_player.current_animation != "idle":
 			animation_player.play("idle")
 		
 		velocity.x = move_toward(velocity.x, 0, move_speed)
